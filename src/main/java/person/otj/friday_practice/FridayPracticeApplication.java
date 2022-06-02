eeea6e8e90f0d0ac7c0145de1d9fda31110a7d10
欧桐江 <3210963178@qq.com>
2022-06-02 10:18:05 +0800

    初始化权限管理系统

---
+++ src/main/java/person/otj/friday_practice/FridayPracticeApplication.java
@@ -0,0 +1,13 @@
+package person.otj.friday_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridayPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FridayPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/person/otj/friday_practice/FridayPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package person.otj.friday_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridayPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
